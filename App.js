4b260260f921282861fb9627bf6964c42c247b9d
Ansruta <75419631+Ansruta@users.noreply.github.com>
2021-11-21 18:55:17 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
+import DrawerNavigator  from './DrawerNavigator';
 
 export default function App() {
   return (
-    <NavigatorContainer>
+    <NavigationContainer>
       <DrawerNavigator/>
-    </NavigatorContainer>
+    </NavigationContainer>
   );
 }
 
